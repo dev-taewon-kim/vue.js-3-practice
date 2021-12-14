410cc428f4515f5a2d140578f4c37347f4a6c19b
dev-taewon-kim <dev.taewon.kim@gmail.com>
2021-12-14 23:19:55 +0900

    feat(Basics & Core Concepts - DOM Interaction with Vue): Assignment-01

---
+++ practice/02/assignment-01/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      my_name: "Taewon Kim",
+      my_age: 22,
+      my_image: "https://test.taewonkim.com/vue.js-3-practice/practice/02/assignment-01/yujin_ahn.jpg",
+    };
+  },
+});
+
+app.mount("#assignment");
