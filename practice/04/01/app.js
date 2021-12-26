6e5bd915535d7cbfef7798d93aef203de5e9b24e
dev-taewon-kim <dev.taewon.kim@gmail.com>
2021-12-26 22:10:34 +0900

    feat(Course Project: The Monster Slayer Game): No.51

---
+++ practice/04/01/app.js
@@ -0,0 +1,25 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHeath: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHeath -= attackValue;
+    },
+  },
+});
+
+app.mount("#game");
