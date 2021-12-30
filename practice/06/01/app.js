0c1901243a6a01e1b6f8ef4ab1ea2a73072a26f1
dev-taewon-kim <dev.taewon.kim@gmail.com>
2021-12-30 23:01:26 +0900

    feat(Vue: Behind the Scenes): No.71

--- practice/06/01/app.js
+++ practice/06/01/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      isDetailsVisible: false,
       friends: [
         {
           id: "ktw",
@@ -16,6 +15,30 @@ const app = Vue.createApp({
           email: "blackpink@gmail.com",
         },
       ],
+    };
+  },
+});
+
+app.component("friend-contact", {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails()">{{ isDetailsVisible ? "Hide" : "Show" }}</button>
+      <ul v-if="isDetailsVisible">
+        <li><strong>Phone:</strong> {{ friend.phone }}</li>
+        <li><strong>Email:</strong> {{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      isDetailsVisible: false,
+      friend: {
+        id: "ktw",
+        name: "Taewon Kim",
+        phone: "010-1234-5678",
+        email: "blablabla@gmail.com",
+      },
     };
   },
   methods: {
