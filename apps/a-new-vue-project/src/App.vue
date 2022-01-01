b485ffd51c8eb9692c9d3294ff2f27be0aa88fc6
dev-taewon-kim <dev.taewon.kim@gmail.com>
2022-01-01 23:53:51 +0900

    feat(Section 7: Vue Cli): No.85

---
+++ apps/a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "ktw",
+          name: "Taewon Kim",
+          phone: "010 1234 5678",
+          email: "blabla@gmail.com",
+        },
+        {
+          id: "jennie",
+          name: "Jennie Kim",
+          phone: "010 0000 0000",
+          email: "blackpink@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
